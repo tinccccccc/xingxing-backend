ed811ee37f95bf218125107215093a0f4c15fbce
jian.wang <571376264@qq.com>
2023-09-03 19:30:01 +0800

    统一异常处理-权限异常

--- web/src/main/java/com/xingxing/controller/login/LoginController.java
+++ web/src/main/java/com/xingxing/controller/login/LoginController.java
@@ -5,32 +5,41 @@ import com.xingxing.authorization.model.User;
 import com.xingxing.commom.model.ResponseResult;
 import com.xingxing.service.authorization.LoginService;
 import com.xingxing.service.authorization.RegisterService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import com.xingxing.service.authorization.UserService;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
-import java.util.Map;
 
 @RestController
+@RequestMapping("/user")
 public class LoginController {
-
     @Resource
     private LoginService loginService;
-
     @Resource
     private RegisterService registerService;
-
+    @Resource
+    private UserService userService;
     /**
      * 用户登录
      *
      * @param user
      * @return
      */
-    @PostMapping("/user/login")
-    public ResponseResult<String> login(@RequestBody User user){
+    @PostMapping("/login")
+    public ResponseResult login(@RequestBody User user){
+        String token = loginService.login(user);
+        return ResponseResult.SUCCESS(token);
+    }
 
-        return loginService.login(user);
+    /**
+     * 用户登出
+     *
+     * @return
+     */
+    @PostMapping("/loginOut")
+    public ResponseResult loginOut(){
+        loginService.logout();
+        return ResponseResult.SUCCESS();
     }
 
     /**
@@ -39,9 +48,15 @@ public class LoginController {
      * @param registerUser
      * @return
      */
-    @PostMapping("/user/register")
-    public ResponseResult<String> register(@RequestBody RegisterUser registerUser){
+    @PostMapping("/register")
+    public ResponseResult register(@RequestBody RegisterUser registerUser){
+        registerService.register(registerUser);
+        return ResponseResult.SUCCESS();
+    }
 
-        return new ResponseResult<>(200,registerService.register(registerUser));
+    @GetMapping("/getUserInfo")
+    public ResponseResult getUserInfo(){
+        User user = userService.getUserInfo();
+        return ResponseResult.SUCCESS(user);
     }
 }
